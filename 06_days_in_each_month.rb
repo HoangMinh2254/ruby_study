8ee385c964f06d9e2559552f83d20af59e4fb848
HoangMinh2254 <socsieuvay2254@gmail.com>
2019-11-15 17:55:59 +0700

    Test commit

--- 06_days_in_each_month.rb
+++ 06_days_in_each_month.rb
@@ -1,17 +1,76 @@
-require "date"
 
-def days_in_month(month)
-    d = Date.new(2, mont8, 28)
-    # See if the year is a leap year.
-    if d.leap?
-        return 30
-    else
-        return 31
-    end
-end
+month_names = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-# Test years for leap status.
-1.upto(12) do |month|
-    puts month.to_s << " has " <<
-        days_in_month(month).to_s << " days"
-end
+print month_names[0]
+print " has "
+print days[0]
+print " days " 
+
+print "\n"
+print month_names[1]
+print " has "
+print days[1]
+print " days " 
+
+print "\n"
+print month_names[2]
+print " has "
+print days[2]
+print " days " 
+
+print "\n"
+print month_names[3]
+print " has "
+print days[3]
+print " days " 
+
+print "\n"
+print month_names[4]
+print " has "
+print days[4]
+print " days " 
+
+print "\n"
+print month_names[5]
+print " has "
+print days[5]
+print " days " 
+
+print "\n"
+print month_names[6]
+print " has "
+print days[6]
+print " days " 
+
+print "\n"
+print month_names[7]
+print " has "
+print days[7]
+print " days " 
+
+print "\n"
+print month_names[8]
+print " has "
+print days[8]
+print " days " 
+
+print "\n"
+print month_names[9]
+print " has "
+print days[9]
+print " days " 
+
+print "\n"
+print month_names[10]
+print " has "
+print days[10]
+print " days " 
+
+print "\n"
+print month_names[11]
+print " has "
+print days[11]
+print " days " 
+
+
